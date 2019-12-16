f3d7f2de790932805acddf5cf6811acf901274e9
hyecheon <rainbow880616@gmail.com>
2019-12-16 21:59:24 +0900

    redux-thunk

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Counter from "../components/Counter";
-import { decrease, increase } from "../modules/counter";
+import {decrease, decreaseAsync, increase, increaseAsync} from "../modules/counter";
 
 const CounterContainer = () => {
   const number = useSelector(state => state.counter);
   const dispatch = useDispatch();
   const onIncrease = () => {
-    dispatch(increase());
+    dispatch(increaseAsync());
   };
   const onDecrease = () => {
-    dispatch(decrease());
+    dispatch(decreaseAsync());
   };
   return (
     <Counter number={number} onDecrease={onDecrease} onIncrease={onIncrease} />
