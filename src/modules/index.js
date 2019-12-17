472692cea491421d3013f605477d7c0a0548f197
hyecheon <rainbow880616@gmail.com>
2019-12-17 20:23:35 +0900

    posts reducer 작성

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,7 @@
-import { combineReducers } from "redux";
+import {combineReducers} from "redux";
 import counter from "./counter";
+import posts from "./posts";
 
-let rootReducer = combineReducers({ counter });
+let rootReducer = combineReducers({counter, posts});
 
 export default rootReducer;
